73fd4307e41ba0496599a697e3eab7ab01b2b123
Aayam <getaayam@gmail.com>
2022-02-19 22:26:03 +0545

    added styles for appbar

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,20 +1,27 @@
-import { makeStyles } from "@mui/styles";
+import { alpha, makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   title: {
     display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
   },
   search: {
     position: "relative",
-    borderRadius: "10px",
-    backgroundColor: "#ccccc",
-    "&:hover": { backgroundColor: "#f6f6f6" },
-    marginRight: "10px",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
   },
   searchIcon: {
-    padding: "10px",
+    padding: theme.spacing(0, 2),
     height: "100%",
     position: "absolute",
     pointerEvents: "none",
@@ -26,9 +33,11 @@ export default makeStyles((theme) => ({
     color: "inherit",
   },
   inputInput: {
-    padding: "1rem",
-    paddingLeft: "1rem",
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
     width: "100%",
+    [theme.breakpoints.up("md")]: { width: "20ch" },
   },
   toolbar: {
     display: "flex",
